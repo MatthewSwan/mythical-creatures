a3fc4d04005e780ef08ac45b7d41bde11d4d2158
Josh Cheek <josh.cheek@gmail.com>
2016-02-16 09:46:01 -0600

    Small changes

--- centaur_array.rb
+++ centaur_array.rb
@@ -1,15 +1,15 @@
 class Centaur
-  NAME     = 0
-  BREED    = 1
-  COUNT    = 2
-  STANDING = 3
+  NAME           = 0
+  BREED          = 1
+  ACTIVITY_COUNT = 2
+  STANDING       = 3
 
   def initialize(name, breed)
-    @options           = []
-    @options[NAME    ] = name
-    @options[BREED   ] = breed
-    @options[COUNT   ] = 0
-    @options[STANDING] = true
+    @options                 = []
+    @options[NAME          ] = name
+    @options[BREED         ] = breed
+    @options[ACTIVITY_COUNT] = 0
+    @options[STANDING      ] = true
   end
 
   def name
@@ -29,24 +29,24 @@ class Centaur
   end
 
   def cranky?
-    3 <= @options[COUNT]
+    3 <= @options[ACTIVITY_COUNT]
   end
 
   def standing?
     @options[STANDING]
   end
 
+  def laying?
+    !standing?
+  end
+
   def sleep
-    @options[COUNT] = 0
+    @options[ACTIVITY_COUNT] = 0
     "NO!" if standing?
   end
 
   def lay_down
     @options[STANDING] = false
-  end
-
-  def laying?
-    !standing?
   end
 
   def stand_up
@@ -56,7 +56,7 @@ class Centaur
   private
 
   def work(sound)
-    @options[COUNT] +=1
+    @options[ACTIVITY_COUNT] += 1
     if cranky? || laying?
       "NO!"
     else
